77a476349060f207262e795f0fd4322dfea3d633
unknown <meerajacob01@gmail.com>
2021-05-28 00:19:09 +0530

    stone,paper,scissor

--- q7.py
+++ q7.py
@@ -1,28 +1,27 @@
-
 import random
 t = ["stone","paper","scissors"]
 comp = t[random.randint(0,2)]
 player = False
 while player == False:
-     player = input("stone,paper,scissors:")
+      player = input("stone,paper,scissors:")
 if player == comp:
     print("tie")
-elif player == stone:
+elif player == 'stone':
 
-    if comp == paper:
+    if comp == 'paper':
         print("you lose" ,comp,"covers",player)
     else:
         print("you won ",player,"covers",comp)
 
-elif player == paper:
+elif player == 'paper':
 
-    if comp == scissors:
+    if comp == 'scissors':
         print("you lose",comp,"covers",player)
     else:
         print("you won ",player,"covers",comp)
 
-elif player == scissors:
-    if comp == stone:
+elif player == 'scissors':
+    if comp == 'stone':
         print("you lose",comp,"covers",player)
     else:
         print("you won ",player,"covers",comp)
